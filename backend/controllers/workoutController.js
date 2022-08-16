37895d43f3bf2cbb6480fe20b5ca99da3c0db398
jsmn-prhmt <jsmnprh@gmail.com>
2022-08-16 20:13:11 +0200

    update backend

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -63,7 +63,7 @@ const deleteWorkout = async (req, res) => {
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({error: 'No such workout'})
-  } 
+  }
 
   const workout = await Workout.findOneAndDelete({_id: id})
 
