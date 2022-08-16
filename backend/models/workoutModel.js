a697cff25c4888f42bf3b3243ed21c9152eb8ebb
jsmn-prhmt <jsmnprh@gmail.com>
2022-08-16 19:54:45 +0200

    update context

--- backend/models/workoutModel.js
+++ backend/models/workoutModel.js
@@ -1,4 +1,4 @@
- const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
@@ -13,6 +13,10 @@ const workoutSchema = new Schema({
   },
   load: {
     type: Number,
+    required: true
+  },
+  user_id: {
+    type: String,
     required: true
   }
 }, { timestamps: true })
