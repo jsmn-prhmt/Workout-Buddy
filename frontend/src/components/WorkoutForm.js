58f60cd9da17e0f155c030b9d0b0bd28e4ca27f6
jsmn-prhmt <jsmnprh@gmail.com>
2022-08-22 19:28:13 +0200

    update

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -50,7 +50,7 @@ const WorkoutForm = () => {
     <form className="create" onSubmit={handleSubmit}>
       <h3>Add a New Workout</h3>
 
-      <label>Excersize Title:</label>
+      <label>Exercise Title:</label>
       <input 
         type="text"
         onChange={(e) => setTitle(e.target.value)}
@@ -58,7 +58,7 @@ const WorkoutForm = () => {
         className={emptyFields.includes('title') ? 'error' : ''}
       />
 
-      <label>Load (in kg):</label>
+      <label>Weight (in kg):</label>
       <input 
         type="number"
         onChange={(e) => setLoad(e.target.value)}
